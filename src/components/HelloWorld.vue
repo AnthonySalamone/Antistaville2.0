800327b1fb0c16706bf1575944bdd3f2e45a1997
Ali DALHOUSS <ali.dalhouss@gmail.com>
2022-12-30 23:46:33 +0100

    Initial commit: Tailwind css and Vite installed, recommanded vscode extension added.

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="mx-auto max-w-screen-md px-6">
+    <h1 class="mt-28 text-4xl font-extrabold text-indigo-500">
+      Tailwind CSS Starter Kit
+    </h1>
+    <h2 class="mt-8 text-2xl font-medium text-slate-700">Hi There 👋 !</h2>
+    <p class="mt-4 mb-10 text-lg leading-6 text-slate-700">
+      This is Tailwind CSS Starter Kit to help you get started quickly with a
+      new Web project using Tailwind CSS installed with Vite.
+    </p>
+    <div class="">
+      <h2
+        class="mb-3 border-b border-slate-100 pb-1 text-3xl font-bold text-slate-700"
+      >
+        Installation
+      </h2>
+      <p class="text-slate-700">To be completed...</p>
+    </div>
+  </div>
+</template>
