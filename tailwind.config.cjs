f8305d6bc1b42a4ab62a9d31f3599fef92ca0efd
Anthony Salamone <anthonysalamone613@gmail.com>
2025-01-24 09:22:23 +0100

    Project init, clean environement, svg sprite, assets

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,6 +5,21 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'purple': '#3f3cbb',
+      'midnight': '#121063',
+      'metal': '#565584',
+      'tahiti': '#3ab7bf',
+      'silver': '#ecebff',
+      'bubble-gum': '#ff77e9',
+      'bermuda': '#78dcca',
+    },
+    fontSize: {
+      'h-3xl': '2rem',
+    },
     extend: {},
   },
   plugins: [],
